383b22c1643f6841c57c732d8eecd057175e8c0a
Durrant Baum <dlbaum12@gmail.com>
2023-02-16 22:36:17 -0700

    Completed all but the extra credit.

---
+++ profile.js
@@ -0,0 +1,14 @@
+let button = document.getElementById("color")
+
+button.addEventListener("click", getColor()){
+    alert('My favorite color is Purple!')
+}
+
+button2.addEventListener("click", getPlace()){
+    alert('My favorite place is home!')
+}
+let button3 = document.getElementById("ritual")
+
+button3.addEventListener("click", getRitual()){
+    alert('My favorite ritual is doing my skin care!')
+}
